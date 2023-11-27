74f8245b6480e624d8122bbff1a2b50c75ac0449
Franco Cabrera <fcabrera.e@cocha.com>
2023-11-26 23:06:39 -0300

    add new app dark mode

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MatButtonModule, MatSlideToggleModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    if(localStorage.getItem('theme')){
+      this.themeColor = localStorage.getItem('theme') as string;
+
+      const body = document.getElementsByTagName('body')[0];
+      body.classList.add(this.themeColor)
+    }
+  }
+  title = 'app';
+  public themeColor: string = 'light-theme';
+
+
+  public changeTheme(): void {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove(this.themeColor);
+
+    this.themeColor === 'dark-theme' ? this.themeColor =  'light-theme' : this.themeColor = 'dark-theme';
+
+    body.classList.add(this.themeColor);
+    localStorage.setItem('theme', this.themeColor);
+  }
+}
